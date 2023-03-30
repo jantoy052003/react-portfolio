028ba5e9f83b1aeec67207da13f0789547e4f1a1
jantoy052003 <124265990+jantoy052003@users.noreply.github.com>
2023-03-30 14:15:47 +0800

    fixed img source

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import sample1 from '../assets/images/sample_project.png'
-import sample2 from '../assets/images/perfume_card.png'
+import sample2 from '../assets/images/perfume_card.jpg'
 import sample3 from '../assets/images/project3.png'
 
 const Projects = () => {
